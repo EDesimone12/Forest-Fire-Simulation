842497398eed208a1ce4db48fb3ad692f3b35c58
eugenio12simone <eugenio12simone@gmail.com>
2022-05-21 11:04:34 +0200

    First Commit

---
+++ src/main.c
@@ -0,0 +1,98 @@
+#include "mpi.h"
+#include "myforest.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]){
+    int my_rank; //Process Rank
+    int size_p; //Max P number
+    int prec; //Processo precednte
+    int dest; //Processo successivo
+    char forest[N][N];
+
+
+    generation(forest);
+
+
+    MPI_Init(&argc,&argv);
+    MPI_Comm_rank (MPI_COMM_WORLD,&my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD,&size_p);
+
+    MPI_Request req = MPI_REQUEST_NULL; //Send
+    MPI_Request req1 = MPI_REQUEST_NULL; //Recv pre
+    MPI_Request req2 = MPI_REQUEST_NULL; //Recv post
+    MPI_Status Stat1;
+    MPI_Status Stat2;
+
+    //Calcolo il numero di elementi per ogni processo
+    int* sendCount = malloc ( size_p* sizeof(int));
+    int* displacement = malloc(size_p * sizeof(int));
+    
+
+
+    if(my_rank == 0){
+        //Stampo la matrice
+        print_forest(forest);
+    }
+    if(my_rank != 0){
+        precDest(my_rank,size_p,&prec,&dest);
+    }
+    divWork(size_p,&sendCount,&displacement);
+    char recvBuff[100];
+
+    MPI_Scatterv(forest,sendCount,displacement,MPI_CHAR,recvBuff,sendCount[my_rank],MPI_CHAR,0,MPI_COMM_WORLD);
+
+    if( my_rank != 0){
+
+        char* preNeighbor = (char*) malloc(sendCount[prec]*sizeof(char));
+        char* destNeighbor = (char*) malloc(sendCount[dest]*sizeof(char));
+
+        if(prec != -10){
+            //Ricevo dal precedente        
+            MPI_Irecv(preNeighbor,sendCount[prec],MPI_CHAR,prec,TAG,MPI_COMM_WORLD,&req1);
+        }
+        //Ricevo dal successivo        
+        if(dest != -10){
+            MPI_Irecv(destNeighbor,sendCount[dest],MPI_CHAR,dest,TAG,MPI_COMM_WORLD,&req2);
+        }
+        //Invio al precedente
+        //printf("Sendocunt[%d]:%d\n",my_rank,sendCount[my_rank]);
+        if(prec != -10){
+            MPI_Isend(recvBuff,sendCount[my_rank],MPI_CHAR,prec,TAG,MPI_COMM_WORLD,&req);
+        }
+        if(dest != -10){
+            //Invio al successivo
+            MPI_Isend(recvBuff,sendCount[my_rank],MPI_CHAR,dest,TAG,MPI_COMM_WORLD,&req);
+        }
+
+        MPI_Wait(&req1,&Stat1);
+        MPI_Wait(&req2,&Stat2);
+
+        int total = 0;
+        char* tempMatrix = prepareForCheck(preNeighbor,recvBuff,destNeighbor, sendCount, my_rank,prec,dest,&total);
+        char*sendBuff = check(&tempMatrix,sendCount,prec,dest,total);
+
+        //printf("Total = %d  Myrank = %d\n",total,my_rank);
+
+        /* Stampa precedenti e successivi per ogni processo
+        if(prec != -10){
+            printf("Sono %d - PRE\n",my_rank);
+            for(int i = 0; i < sendCount[prec]; i++){
+                printf("%c ",preNeighbor[i]);
+            }
+            printf("\n");
+        }
+        if(dest != -10){
+            printf("Sono %d - DEST\n",my_rank);
+            for(int i = 0; i < sendCount[dest]; i++){
+                printf("%c ",destNeighbor[i]);
+            }
+            printf("\n");
+        }*/
+    }
+
+    MPI_Finalize();
+    
+}
---
+++ src/myforest.h
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAG 42
+#define N 10   //Matrix Dimension
+#define F 10    //Ignite probability F(Fire)
+#define P 70       //New Tree probability 
+
+void generation(char matrix[N][N]){
+    // 1(TREE) 2(EMPTY) 3(BURNING TREE)
+
+    /*matrix = (char **)malloc(N*sizeof(char *)); //Array of pointer
+    matrix[0] = (char *)malloc(N*N*sizeof(char)); //Matrix
+
+    for(int i=1; i < N; i++)
+        matrix[i] = matrix[0] + i*N; */
+
+    for(int i=0; i < N; i++){
+        for(int j =0; j < N; j++){
+            if((rand() %101) <= P){
+                matrix[i][j] = '1';
+            }else{
+                matrix[i][j] = '2';
+            }
+        }
+    }
+}
+
+void print_forest(char f[N][N]){
+    for(int i=0; i < N; i++){
+        printf("--------------------------------------------------\n");
+        for(int j=0; j < N; j++){
+            printf("| %c |",f[i][j]);
+        }
+        printf("\n");
+    }
+    printf("--------------------------------------------------\n");
+}
+
+void precDest (int my_rank, int size_p, int *prec , int* dest){
+    if(my_rank == 1){
+        *prec = -10 ; //Il primo processo non ha precedenti
+    }else{
+        *prec = (my_rank + size_p -1) % size_p;
+    }
+    if(my_rank == (size_p-1) ){
+        *dest = -10; //L'ultimo processo non ha successivi
+    }else{
+        *dest = ((my_rank + 1) % size_p);
+    }
+    //printf("Sono %d , pre: %d  dest:%d \n",my_rank,*prec,*dest);
+}
+
+void divWork(int size,int** sendCount, int** displacement){
+    //printf("Inizio divisione...\n");
+    int divVal = (N*N) / (size-1);
+    int restVal = (N*N) % (size-1);
+
+    int pos = 0;
+    displacement[0][0] = 0;
+    sendCount[0][0] = 0; //Al processo master non dò nulla da fare.
+
+    for(int i = 1; i < size;i++){
+        sendCount[0][i] = divVal;
+        if(restVal > 0){
+            sendCount[0][i]++;
+            restVal--;
+        }
+        //Calcolo il displacement per la scatterv
+        displacement[0][i] = pos;
+        pos = pos + sendCount[0][i];
+        //printf("Displ[%d]:%d \n",i,displacement[0][i]);
+        //printf("SendCount[%d]: %d \n ",i,sendCount[0][i]);
+    }
+    //printf("Fine divisione...\n");
+}
+
+char* prepareForCheck(char* preNeighbor,char* recvBuff,char* destNeighbor,int* sendCount,int my_rank,int prec, int dest,int* total){
+        char *arr = malloc(sizeof *arr * N * N);
+
+        if(prec != -10){
+            memcpy(arr,preNeighbor,sendCount[prec]);
+            *total += sendCount[prec];
+            memcpy(arr+sendCount[prec],recvBuff,sendCount[my_rank]);
+            *total += sendCount[my_rank];
+            //printf("rank= %d - sendCount[my_rank] %d - sendCount[dest] = %d  dest= %d\n",my_rank,sendCount[my_rank],sendCount[dest],dest);
+            if(dest != -10){
+                memcpy(arr+sendCount[prec]+sendCount[my_rank],destNeighbor, sendCount[dest]);
+                *total += sendCount[dest];
+            }
+        }else{
+            memcpy(arr, recvBuff, sendCount[my_rank]);
+            *total += sendCount[my_rank];
+            //printf("sotto - rank= %d - sendCount[my_rank] %d - sendCount[dest] = %d  dest= %d  prec = %d\n",my_rank,sendCount[my_rank],sendCount[dest],dest,prec);
+            memcpy(arr+sendCount[my_rank],destNeighbor,sendCount[dest]);
+            *total += sendCount[dest];
+        }
+        /* //Stampa per verifica della creazione dela matrice temporanea
+        int count = 0;
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                printf("(rank = %d i=%d j=%d count=%d) -elem [%d] = %c\n",my_rank,i,j,count,((i*N)+j),arr[((i*N)+j)]);
+                count++;
+            }
+            printf("------\n");
+        }*/
+        return arr;
+}
+
+char* check(char** temp, char* sendCount, int prec, int dest, int total){
+    int start = 0;
+
+    //Starting point
+    if(prec == -10){
+        start = 0;
+    }else{
+        start = sendCount[prec];
+    }
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++ ){
+
+        }
+    }
+}
